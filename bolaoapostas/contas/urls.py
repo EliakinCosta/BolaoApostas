a2cab63c15d4589f640f10ff8776c9c96bf43179
EliakinCosta <eliakim170@gmail.com>
2016-04-07 20:33:16 -0300

    Jogador faz login e logout

--- bolaoapostas/contas/urls.py
+++ bolaoapostas/contas/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     # O dicionario serve para os parametros que serao passados por parametro para a view de login
     url(r'^entrar/$', 'django.contrib.auth.views.login', {'template_name':'contas/login.html'}, name='login'),
+    url(r'^sair/$', 'django.contrib.auth.views.logout', {'next_page':'administracao:inicio'}, name='logout'),
 ]

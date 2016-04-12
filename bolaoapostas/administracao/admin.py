cc3cf01663a76c26626d1a863ad71acb84abd7b7
Fernando Telles <fernandotalves@gmail.com>
2016-04-12 23:58:28 +0000

    Logica de divisao da premiacao

--- bolaoapostas/administracao/admin.py
+++ bolaoapostas/administracao/admin.py
@@ -22,10 +22,69 @@ class JogadorAdmin(admin.ModelAdmin):
         obj.user.delete()
         obj.delete()
 
-
+class PartidaAdmin(admin.ModelAdmin):
+    """ """
+    def save_model(self, request, obj, form, change):
+        if obj:
+            if obj.status.id == 2:
+                ganhadores, quantidade_apostas = self.get_ganhadores(obj)
+                
+                for ganhador in ganhadores:
+                    ganhador.saldo = float(ganhador.saldo) + float((quantidade_apostas * 5) / len(ganhadores))
+                    ganhador.save()
+        obj.save()
+    
+    def get_ganhadores(self, partida):
+        apostas = Aposta.objects.all().filter(id_partida = partida)
+        
+        #Verificar apostas_premiadas por placar
+        apostas_premiadas = []
+        
+        ganhadores = []
+        
+        for aposta in apostas:
+            if (aposta.gols_time_casa == partida.gols_casa and 
+                aposta.gols_time_visitante == partida.gols_visitante):
+                apostas_premiadas.append(aposta)
+        else:
+            if not apostas_premiadas:
+                for aposta in apostas:
+                    if self.get_resultado_aposta(aposta) == self.get_resultado_partida(partida):
+                        apostas_premiadas.append(aposta)
+                else:
+                    if not apostas_premiadas:
+                        apostas_premiadas = list(apostas)
+        
+        if apostas_premiadas:
+            for aposta_premiada in apostas_premiadas:
+                ganhadores.append(aposta_premiada.apostador)
+        
+        return ganhadores, len(apostas)
+        
+        
+    #
+    def get_resultado_aposta(self, aposta):
+        resultado = aposta.gols_time_casa - aposta.gols_time_visitante
+        
+        if resultado > 0:
+            return 1
+        elif resultado < 0:
+            return -1
+        else:
+            return 0
+            
+    def get_resultado_partida(self, aposta):
+        resultado = partida.gols_casa - partida.gols_visitante
+        
+        if resultado > 0:
+            return 1
+        elif resultado < 0:
+            return -1
+        else:
+            return 0
 
 admin.site.register(Time)
-admin.site.register(Partida)
+admin.site.register(Partida, PartidaAdmin)
 admin.site.register(Aposta)
 admin.site.register(StatusPartida)
 admin.site.register(Jogador, JogadorAdmin)

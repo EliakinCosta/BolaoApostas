ee461e07e4d891c9f25e0b4f363d07b4d30db644
EliakinCosta <eliakim170@gmail.com>
2016-04-18 12:55:34 -0300

    Bloqueio do botao apos a aposta e ao carregar a pagina

--- bolaoapostas/contas/views.py
+++ bolaoapostas/contas/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from administracao.models import *
-from decimal import Decimal
-import json
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
 
 # Create your views here.
 
@@ -12,12 +9,20 @@ from django.shortcuts import render_to_response
 def painel(request):
     template_name = 'contas/partidas.html'
     partidas = Partida.objects.all()
-    return render(request, template_name, {'jogador': queryset_Jogador(request.user), 'partidas': partidas})
+    return render(request, template_name, {'jogador': queryset_Jogador(request.user),
+                                           'partidas': partidas,
+                                           'partidas_jogador':get_partidas_apostadas(request.user)})
 
 
 def queryset_Jogador(usuario):
     return Jogador.objects.get(usuario=usuario)
 
+def get_partidas_apostadas(usuario):
+    apostas = Aposta.objects.filter(apostador=queryset_Jogador(usuario))
+    partidas = []
+    for aposta in apostas:
+        partidas.append(aposta.id_partida)
+    return partidas
 
 def apostar(request):
     if request.method == 'POST':
@@ -41,7 +46,6 @@ def apostar(request):
                  valor=-5.00,
             )
 
-            sacar_aposta(jogador)
             response_data = {}
             response_data['result'] = 'Create post successful!'
             response_data['jogador'] = float(jogador.saldo)
